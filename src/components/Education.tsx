f24c9f764c8027e8ba250c77919e3b2aa58953a4
Jake <jakem5531@gmail.com>
2021-10-27 22:35:22 -0700

    Added Project and Experience

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -2,7 +2,7 @@ import { Typography, Avatar, Container, Grid } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
-  experience: {
+  education: {
     paddingTop: 500
   },
   title: {
@@ -14,8 +14,34 @@ function Education() {
   const classes = useStyles();
 
   return (
-    <Container className={classes.experience}>
-      <Typography variant="h3" className={classes.title}>Education</Typography>
+    <Container className={classes.education}>
+      <Typography variant="h3" className={classes.title}>Experience</Typography>
+      <Grid container alignItems="center" justifyContent="center" direction="column" spacing={4}>
+        <Grid item>
+          <Avatar alt="AWS" src={`${process.env.PUBLIC_URL}/UBC.jpg`} sx={{ width: 80, height: 80 }}/>
+        </Grid>
+        <Grid item>
+          <Typography variant="h4">
+            University of British Columbia
+          </Typography>
+          <Typography>
+            BSc Computer Science
+            (2018 - 2022)
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Avatar alt="SAP" src={`${process.env.PUBLIC_URL}/UofT.jpg`} sx={{ width: 80, height: 80 }}/>
+        </Grid>
+        <Grid item>
+        <Typography variant="h4">
+            University of Toronto
+          </Typography>
+          <Typography>
+            BSc Animal Physiology & Human Physiology
+            (2012 - 2017)
+          </Typography>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,4 @@
-import { Typography, Avatar, Container, Grid } from '@mui/material';
+import { Typography, Button, Container, Grid, Card, CardMedia, CardContent, CardActions } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
@@ -7,6 +7,13 @@ const useStyles = makeStyles({
   },
   title: {
     paddingBottom: 100
+  },
+  justifyContent: {
+    display: "flex",
+    justifyContent: "flex-end"
+  },
+  rightAlignItem: {
+    marginLeft: "auto"
   }
 })
 
@@ -16,6 +23,142 @@ function Project() {
   return (
     <Container className={classes.experience}>
       <Typography variant="h3" className={classes.title}>Project</Typography>
+      <Grid container spacing={4}>
+        <Grid item xs={4}>
+          <Card>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/jakemoh.jpg"
+              alt="sample"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5">
+                Game 1
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque egestas auctor finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.justifyContent}>
+              <Button variant="contained" size="medium" className={classes.rightAlignItem}>Play</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Card>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/jakemoh.jpg"
+              alt="sample"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5">
+                Game 1
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque egestas auctor finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.justifyContent}>
+              <Button variant="contained" size="medium" className={classes.rightAlignItem}>Play</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Card>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/jakemoh.jpg"
+              alt="sample"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5">
+                Game 1
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque egestas auctor finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.justifyContent}>
+              <Button variant="contained" size="medium" className={classes.rightAlignItem}>Play</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Card>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/jakemoh.jpg"
+              alt="sample"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5">
+                Game 1
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque egestas auctor finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.justifyContent}>
+              <Button variant="contained" size="medium" className={classes.rightAlignItem}>Play</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Card>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/jakemoh.jpg"
+              alt="sample"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5">
+                Game 1
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque egestas auctor finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.justifyContent}>
+              <Button variant="contained" size="medium" className={classes.rightAlignItem}>Play</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Card>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/jakemoh.jpg"
+              alt="sample"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5">
+                Game 1
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque egestas auctor finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.justifyContent}>
+              <Button variant="contained" size="medium" className={classes.rightAlignItem}>Play</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        
+      </Grid>
+
+      
     </Container>
   );
 }
