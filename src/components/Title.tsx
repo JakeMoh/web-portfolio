0dab5219063db3f4ff2d16edc900b6dd982d45b6
Jake <jakem5531@gmail.com>
2021-10-27 23:08:12 -0700

    GitHub and LinkedIn Icon

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,14 @@
 import {Avatar, Typography, Container, Grid} from '@mui/material';
 import { makeStyles } from '@mui/styles';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const useStyles = makeStyles({
   title: {
     paddingTop: "30vh"
+  },
+  icon: {
+    padding: 10
   }
 })
 
@@ -16,8 +21,9 @@ function Title() {
         <Avatar alt="Jake Moh" src={`${process.env.PUBLIC_URL}/jakemoh.jpg`} sx={{ width: 100, height: 100 }}/>
       </Grid>
       <Typography variant="h1">Jake Moh</Typography>
-      <Typography>Personal Web Portfolio</Typography>
       <Typography>UBC CS Year 4</Typography>
+      <GitHubIcon sx={{ fontSize: 60 }} onClick={() => window.open("https://github.com/JakeMoh")} className={classes.icon} />
+      <LinkedInIcon sx={{ fontSize: 60 }} onClick={() => window.open("https://www.linkedin.com/in/jake-moh-1a48a2193/")} className={classes.icon} />
     </Container>
   );
 }
